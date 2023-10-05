3271e73d6449fff53f27830c277778ebdf4c4c1e
arshroop26 <118218321+arshroop26@users.noreply.github.com>
2023-10-05 14:25:22 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"] })
 })
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.listen(5002, () => {console.log("server started on port 5000")})
