5e1272690bc071ada85da805d7688b8b0059b2ce
Manav Patel <64374123+mpat247@users.noreply.github.com>
2023-10-07 13:28:04 -0400

    nodemon

--- server/index.js
+++ server/index.js
@@ -5,4 +5,6 @@ app.get("/api", (req, res) => {
     res.json({"1": ["2", "3", "4"] })
 })
 
-app.listen(5001, () => {console.log("server started on port 5001")})
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));  
