43fb21df0c091e09d7383a9fba697880be23f112
Manav Patel <64374123+mpat247@users.noreply.github.com>
2023-10-07 12:57:55 -0400

    setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"] })
+    res.json({"1": ["2", "3", "4"] })
 })
 
-app.listen(5002, () => {console.log("server started on port 5000")})
+app.listen(5001, () => {console.log("server started on port 5001")})
