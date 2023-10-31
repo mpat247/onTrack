cd3193fed96b780aeddb34b22c8b9ce06c8c8792
Patrick <patzesantoso@gmail.com>
2023-10-31 18:33:25 -0400

    FRONTEND pages
    Made it so that we can access different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Calendar from './CalendarPage.js';
+import Main from './MainPage.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/calendarpage" element={<Calendar />} />
+        <Route index element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/MainPage.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div>
+      {<p>apple</p>}
+    </div>
+  );
+}
+
+export default MainPage;
